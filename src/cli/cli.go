4270fd61312737ba5ba25106ce966244598b3024
tejas <tejashkrishna@gmail.com>
2018-04-15 18:44:54 -0700

    Initial commit

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -1,5 +1,5 @@
 /*
-* Cli for the go file system 
+* Cli for the go file system
 */
 
 package main
@@ -9,31 +9,31 @@ import "os"
 
 func main() {
 
-	for {
-		fmt.Print("##> ")
-		var input string
-		
-		fmt.Scanln(&input)
+    for {
+        fmt.Print("##> ")
+        var input string
 
-		switch input {
-			case "h":
-				helpList()
-			case "quit":
-				os.Exit(0)
-			case "q":
-				os.Exit(0)
-			case "ls":
-				//listFiles()
-			default:
-				fmt.Print(input + " ")
-				fmt.Println("unrecognized conmmand line option")
-				//panic("unrecognized input value")
-		}
-	}
-	//fmt.Println(sum)
+        fmt.Scanln(&input)
+
+        switch input {
+            case "h":
+                helpList()
+            case "quit":
+                os.Exit(0)
+            case "q":
+                os.Exit(0)
+            case "ls":
+                //listFiles()
+            default:
+                fmt.Print(input + " ")
+                fmt.Println("unrecognized conmmand line option")
+                //panic("unrecognized input value")
+        }
+    }
+    //fmt.Println(sum)
 }
 
 func helpList() {
-	fmt.Println(" h  -- help ")
-	fmt.Println(" ls -- list files ")
+    fmt.Println(" h  -- help ")
+    fmt.Println(" ls -- list files ")
 }
